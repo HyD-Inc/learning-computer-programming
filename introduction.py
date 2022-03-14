2562310fc55246e6a7e34fddf683729682508a51
8lurry <sharifmehedi24@outlook.com>
2022-03-14 19:05:12 +0600

    first commit

---
+++ introduction.py
@@ -0,0 +1,19 @@
+"""
+Types of programming language
+=============================
+
+- Compiled language
+- Interpreted language
+
+Compiled language
+-----------------
+
+Has compiler(s), there maybe many compilers provided by different vandors.
+
+An example language is kotlin. Android studio has full functionalities related
+to building/compiling kotlin programs. As said above, there maybe other
+compilers from different vardors.
+
+A compiler usually turns the compiled program into binary packages.
+
+"""
